772131bc79d100ca347abaf58a9cd7877a01ada9
hatc1984 <hatc1984@gmail.com>
2017-06-21 18:33:23 -0500

    UPDATE SHOPPING CART

--- src/main/java/edu/mum/coffee/controller/CartController.java
+++ src/main/java/edu/mum/coffee/controller/CartController.java
@@ -1,20 +1,29 @@
 package edu.mum.coffee.controller;
 
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.mum.coffee.domain.CartInfo;
 import edu.mum.coffee.domain.Product;
 import edu.mum.coffee.domain.ProductInfo;
+import edu.mum.coffee.domain.User;
 import edu.mum.coffee.service.ProductService;
+import edu.mum.coffee.service.UserService;
 import edu.mum.coffee.util.Utils;
 
 @Controller
@@ -23,9 +32,12 @@ public class CartController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private UserService userService;
+	
    @RequestMapping({ "/buyProduct" })
    public String listProductHandler(HttpServletRequest request, Model model, 
-		   @RequestParam(value = "code") int code) {
+		   @RequestParam(value = "code") int code,Principal princial) {
  
        Product product = null;
        if (code>0) {
@@ -37,6 +49,9 @@ public class CartController {
            ProductInfo productInfo = new ProductInfo(product);
  
            cartInfo.addProduct(productInfo, 1);
+           
+           User user = userService.findUserByEmail(princial.getName());
+           cartInfo.setCustomerInfo(user);
        }
     
        return "redirect:/shoppingCart";
@@ -84,5 +99,112 @@ public class CartController {
        model.addAttribute("cartForm", myCart);
        return "shoppingCart";
    }
+   
+   
+   // GET: Nhập thông tin khách hàng.
+   @RequestMapping(value = { "/checkout" }, method = RequestMethod.GET)
+   public String shoppingCartCustomerForm(HttpServletRequest request, Model model) {
+ 
+       CartInfo cartInfo = Utils.getCartInSession(request);
+    
+     
+       // Chưa mua mặt hàng nào.
+       if (cartInfo.isEmpty()) {
+
+           // Chuyển tới trang danh giỏ hàng
+           return "redirect:/shoppingCart";
+       }
+ 
+       User customerInfo = cartInfo.getCustomerInfo();
+       if (customerInfo == null) {
+           customerInfo = new User();
+       }
+ 
+       model.addAttribute("checkoutForm", customerInfo);
+       
+       CartInfo myCart = Utils.getCartInSession(request);
+       model.addAttribute("cartForm", myCart);
+ 
+       return "checkout";
+   }
+ 
+  
+   // POST: Save thông tin khách hàng.
+   @RequestMapping(value = { "/checkout" }, method = RequestMethod.POST)
+   public String shoppingCartCustomerSave(HttpServletRequest request, //
+           Model model, //
+           BindingResult result, //
+           final RedirectAttributes redirectAttributes) {
+ 
+       return "redirect:/shoppingCartConfirmation";
+   }
+ 
+
+   @RequestMapping(value = { "/shoppingCartConfirmation" }, method = RequestMethod.GET)
+   public String shoppingCartConfirmationReview(HttpServletRequest request, Model model) {
+       CartInfo cartInfo = Utils.getCartInSession(request);
+ 
+    
+       if (cartInfo.isEmpty()) {
+         
+           return "redirect:/shoppingCart";
+       } else if (!cartInfo.isValidCustomer()) {
+          
+           return "redirect:/shoppingCartCustomer";
+       }
+ 
+       return "shoppingCartConfirmation";
+   }
+ 
+   @RequestMapping(value = { "/shoppingCartConfirmation" }, method = RequestMethod.POST)
+   @Transactional(propagation = Propagation.NEVER)
+   public String shoppingCartConfirmationSave(HttpServletRequest request, Model model) {
+       CartInfo cartInfo = Utils.getCartInSession(request);
+ 
+    
+       if (cartInfo.isEmpty()) {  
+           return "redirect:/shoppingCart";
+       } else if (!cartInfo.isValidCustomer()) {
+        
+           return "redirect:/checkout";
+       }
+       try {
+           //orderDAO.saveOrder(cartInfo);   // TODO: Save order to database
+       } catch (Exception e) {
+        
+           return "shoppingCartConfirmation";
+       }
+      
+       // Delete cart from Session
+       Utils.removeCartInSession(request);
+        
+   
+       // Save order
+       Utils.storeLastOrderedCartInSession(request, cartInfo);
+ 
+    
+       // Finish.
+       return "redirect:/shoppingCartFinalize";
+   }
+ 
+   @RequestMapping(value = { "/shoppingCartFinalize" }, method = RequestMethod.GET)
+   public String shoppingCartFinalize(HttpServletRequest request, Model model) {
+ 
+       CartInfo lastOrderedCart = Utils.getLastOrderedCartInSession(request);
+ 
+       if (lastOrderedCart == null) {
+           return "redirect:/shoppingCart";
+       }
+ 
+       return "shoppingCartFinalize";
+   }
+   
+   
+   
+   
+   
+   
+   
+   
 	
 }
