651f85d10cafe3ef520fab1d20a1d4b9c93697eb
hatc1984 <hatc1984@gmail.com>
2017-06-21 10:35:36 -0500

    Add files via upload

---
+++ src/main/java/edu/mum/coffee/domain/CartInfo.java
@@ -0,0 +1,121 @@
+package edu.mum.coffee.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class CartInfo {
+ 
+    private int orderNum;
+ 
+    private User customerInfo;
+ 
+    private final List<CartLineInfo> cartLines = new ArrayList<CartLineInfo>();
+ 
+    public CartInfo() {
+ 
+    }
+ 
+    public int getOrderNum() {
+        return orderNum;
+    }
+ 
+    public void setOrderNum(int orderNum) {
+        this.orderNum = orderNum;
+    }
+ 
+    public User getCustomerInfo() {
+        return customerInfo;
+    }
+ 
+    public void setCustomerInfo(User customerInfo) {
+        this.customerInfo = customerInfo;
+    }
+ 
+    public List<CartLineInfo> getCartLines() {
+        return this.cartLines;
+    }
+ 
+    private CartLineInfo findLineByCode(int code) {
+        for (CartLineInfo line : this.cartLines) {
+            if (line.getProductInfo().getId()==code) {
+                return line;
+            }
+        }
+        return null;
+    }
+ 
+    public void addProduct(ProductInfo productInfo, int quantity) {
+        CartLineInfo line = this.findLineByCode(productInfo.getId());
+ 
+        if (line == null) {
+            line = new CartLineInfo();
+            line.setQuantity(0);
+            line.setProductInfo(productInfo);
+            this.cartLines.add(line);
+        }
+        int newQuantity = line.getQuantity() + quantity;
+        if (newQuantity <= 0) {
+            this.cartLines.remove(line);
+        } else {
+            line.setQuantity(newQuantity);
+        }
+    }
+ 
+    public void validate() {
+ 
+    }
+ 
+    public void updateProduct(int code, int quantity) {
+        CartLineInfo line = this.findLineByCode(code);
+
+        if (line != null) {
+            if (quantity <= 0) {
+                this.cartLines.remove(line);
+            } else {
+                line.setQuantity(quantity);
+            }
+        }
+    }
+ 
+    public void removeProduct(ProductInfo productInfo) {
+        CartLineInfo line = this.findLineByCode(productInfo.getId());
+        if (line != null) {
+            this.cartLines.remove(line);
+        }
+    }
+ 
+    public boolean isEmpty() {
+        return this.cartLines.isEmpty();
+    }
+ 
+    public boolean isValidCustomer() {
+        return this.customerInfo != null && this.customerInfo.isEnable();
+    }
+ 
+    public int getQuantityTotal() {
+        int quantity = 0;
+        for (CartLineInfo line : this.cartLines) {
+            quantity += line.getQuantity();
+        }
+        return quantity;
+    }
+ 
+    public double getAmountTotal() {
+        double total = 0;
+        for (CartLineInfo line : this.cartLines) {
+            total += line.getAmount();
+        }
+        return total;
+    }
+ 
+    public void updateQuantity(CartInfo cartForm) {
+        if (cartForm != null) {
+            List<CartLineInfo> lines = cartForm.getCartLines();
+            for (CartLineInfo line : lines) {
+                this.updateProduct(line.getProductInfo().getId(), line.getQuantity());
+            }
+        }
+ 
+    }
+ 
+}
---
+++ src/main/java/edu/mum/coffee/domain/ProductInfo.java
@@ -0,0 +1,112 @@
+package edu.mum.coffee.domain;
+
+import javax.persistence.Entity;
+
+
+public class ProductInfo {
+
+	private int id;
+
+	private String productName;
+
+	private String description;
+
+	private double price;
+
+	private ProductType productType;
+
+	private int quantity;
+
+	private ManuFacturerType manufacturer;
+	
+	private boolean newProduct=false;
+
+	public ProductInfo() {
+		super(); // default constructor
+	}
+
+	public ProductInfo(int id, String productName, String description, double price, ProductType productType,
+			int quantity, ManuFacturerType manufacturer) {
+		this.id = id;
+		this.productName = productName;
+		this.description = description;
+		this.price = price;
+		this.productType = productType;
+		this.quantity = quantity;
+		this.manufacturer = manufacturer;
+	}
+	
+    public ProductInfo(Product product) {
+		this.id = product.getId();
+		this.productName = product.getProductName();
+		this.description = product.getDescription();
+		this.price = product.getPrice();
+		this.productType = product.getProductType();
+		this.quantity = product.getQuantity();
+		this.manufacturer = product.getManufacturer();
+    }
+
+	public ProductType getProductType() {
+		return productType;
+	}
+
+	public void setProductType(ProductType productType) {
+		this.productType = productType;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public ManuFacturerType getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(ManuFacturerType manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+    public boolean isNewProduct() {
+        return newProduct;
+    }
+ 
+    public void setNewProduct(boolean newProduct) {
+        this.newProduct = newProduct;
+    }
+}
